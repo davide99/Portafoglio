21e365bc2a42f00ff133d640d98913d13a67f755
AntonioDeMatteis <dematteisantonio@hotmail.it>
2017-11-16 11:41:03 +0100

    eliminazione commento di prova

--- www/js/app.js
+++ www/js/app.js
@@ -13,7 +13,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-//prova
+
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
