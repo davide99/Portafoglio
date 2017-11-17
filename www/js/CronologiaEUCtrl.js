688d700bd99dca1fcabf650bbc60611ffd392de0
FrancescoToma <frenxis.fra99@gmail.com>
2017-11-17 14:47:29 +0100

    Crono9

--- www/js/CronologiaEUCtrl.js
+++ www/js/CronologiaEUCtrl.js
@@ -1,5 +1,17 @@
-
 angular.module('starter.controllers')
 .controller('CronologiaEUCtrl', function($scope, $http) {
-console.log("Tutto");
+  var link = "http://portafoglio.altervista.org/getCronologia.php";
+  $scope.movimenti = null;
+
+  $http.get(link,{
+    params: {
+      id_utente:1
+      
+    }
+  }).then(function(response){
+    $scope.movimenti = response.data.movimenti;
+    console.log($scope.movimenti);
+  }).catch(function(error){
+    console.log(error);
+  });
 })
--- www/js/CronologiaUsciteCtrl.js
+++ www/js/CronologiaUsciteCtrl.js
@@ -1,6 +1,5 @@
 angular.module('starter.controllers')
 .controller('CronologiaUsciteCtrl', function($scope, $http) {
-  console.log("Entrate");
   var link = "http://portafoglio.altervista.org/select.php";
   $scope.uscite = null;
 
