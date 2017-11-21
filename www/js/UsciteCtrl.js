715f0357f3fff64a322a7f5b5c80fe900f99396d
AntonioDeMatteis <dematteisantonio@hotmail.it>
2017-11-21 12:05:06 +0100

    u

--- www/js/UsciteCtrl.js
+++ www/js/UsciteCtrl.js
@@ -1,5 +1,35 @@
 
 angular.module('starter.controllers')
 .controller('UsciteCtrl', function($scope, sharedProperties) {
-$scope.id_utente=sharedProperties.getIdUtente();
-});
+  //funzione
+  $scope.inserisci = function(){
+    $scope.id_utente=sharedProperties.getIdUtente();
+
+    var link1 = "http://portafoglio.altervista.org/update.php";
+    var link2= "http://portafoglio.altervista.org/select.php";
+
+    $scope.entrate=null;
+    $scope.importo = 0;
+
+    $http.get(link2,{
+      params: {
+        tabella:'utenti',
+        id_utente: $scope.id_utente
+      }
+      }).then(function(response){
+        var saldo = response.data.utenti.saldo;
+        var nuovo_saldo = saldo + $scope.importo;
+        console.log('nuovo_saldo',$scope.importo);
+        console.log('saldo',response.data);
+
+        $http.get(link1,{
+          params: {
+            tabella: 'entrate',
+            importo:  nuovo_saldo
+          }
+        }).then(function(response){
+          console.log(response.data);
+      })
+        })
+      }
+  });
