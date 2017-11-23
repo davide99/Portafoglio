246fe92455da909597ebfbce3e2608955120798c
FrancescoToma <frenxis.fra99@gmail.com>
2017-11-23 10:26:23 +0100

    ricerca e shared

--- www/js/CronologiaUsciteCtrl.js
+++ www/js/CronologiaUsciteCtrl.js
@@ -1,12 +1,13 @@
 angular.module('starter.controllers')
-.controller('CronologiaUsciteCtrl', function($scope, $http) {
-
+.controller('CronologiaUsciteCtrl', function($scope, $http, sharedProperties) {
+$scope.id_utente = sharedProperties.getIdUtente();
   var link = "http://portafoglio.altervista.org/select.php";
   $scope.uscite = null;
 
   $http.get(link,{
     params: {
-      tabella: 'entarte'
+      tabella: 'uscite',
+      id_utente: $scope.id_utente
     }
   }).then(function(response){
     $scope.uscite = response.data.uscite;
@@ -38,7 +39,7 @@ angular.module('starter.controllers')
         var ora=o[i];
 
         $scope.uscite[i].data=data+", "+ora;
-      
+
       }
 
     }).catch(function(error){
