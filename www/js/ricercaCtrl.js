f496281e5e628578195c01616b998590564112a1
FrancescoToma <frenxis.fra99@gmail.com>
2017-11-24 11:04:06 +0100

    ricerca definitiva

--- www/js/ricercaCtrl.js
+++ www/js/ricercaCtrl.js
@@ -1,6 +1,9 @@
+
 angular.module('starter.controllers')
-.controller('ricercaCtrl', function($scope, $http) {
+.controller('ricercaCtrl', function($scope, $http, ionicDatePicker) {
   var link = "http://portafoglio.altervista.org/select.php";
+  var link1 = "http://portafoglio.altervista.org/ricercaPerTipo.php";
+  var link2 = "http://portafoglio.altervista.org/getCronologia.php";
   $scope.categorie = null;
   $scope.visibile="none";
   $scope.cat="Ricerca per categoria...";
@@ -27,17 +30,60 @@ angular.module('starter.controllers')
     console.log(error);
   });
 
-  $scope.carica=function(){
+
+
+  // $scope.caricaPerTipo=function(){
+  //   if($scope.visibile=='block'){
+  //     $scope.visibile='none';
+  //   }
+  //
+  //   $scope.movimenti = null;
+  //
+  //   $http.get(link1,{
+  //     params: {
+  //       id_utente:1,
+  //       id_tipo: $scope.id_tipo
+  //
+  //
+  //     }
+  //   }).then(function(response){
+  //     $scope.movimenti = response.data.movimenti;
+  //     for (var i = 0; i < $scope.movimenti.length; i++) {
+  //       d[i]=$scope.movimenti[i].data.substring(0, 10);
+  //       o[i]=$scope.movimenti[i].data.substring(11, 16);
+  //       var parts = d[i].split("-"),
+  //       dateLong = new Date(+parts[0], parts[1]-1, +parts[2]);
+  //
+  //       var data=dateLong.toString().substring(0,15);
+  //       var ora=o[i];
+  //
+  //       $scope.movimenti[i].data=data+", "+ora;
+  //
+  //     }
+  //
+  //     console.log($scope.movimenti);
+  //   }).catch(function(error){
+  //     console.log(error);
+  //   });
+  // }
+
+  $scope.caricaPerData=function(){
     if($scope.visibile=='block'){
       $scope.visibile='none';
     }
-    var link1 = "http://portafoglio.altervista.org/ricercaPerTipo.php";
+
     $scope.movimenti = null;
 
-    $http.get(link1,{
+    if($scope.cat=="Ricerca per categoria..."){
+
+
+
+    $http.get(link2,{
       params: {
         id_utente:1,
-        id_tipo: $scope.id_tipo
+        anno:  $scope.data.getUTCFullYear(),
+        mese: $scope.data.getUTCMonth()+1,
+        giorno: $scope.data.getUTCDate()
 
 
       }
@@ -60,7 +106,94 @@ angular.module('starter.controllers')
     }).catch(function(error){
       console.log(error);
     });
+  }else if($scope.data=="Ricerca per data..."){
+
+      $http.get(link1,{
+        params: {
+          id_utente:1,
+          id_tipo: $scope.id_tipo
+
+
+        }
+      }).then(function(response){
+        $scope.movimenti = response.data.movimenti;
+        for (var i = 0; i < $scope.movimenti.length; i++) {
+          d[i]=$scope.movimenti[i].data.substring(0, 10);
+          o[i]=$scope.movimenti[i].data.substring(11, 16);
+          var parts = d[i].split("-"),
+          dateLong = new Date(+parts[0], parts[1]-1, +parts[2]);
+
+          var data=dateLong.toString().substring(0,15);
+          var ora=o[i];
+
+          $scope.movimenti[i].data=data+", "+ora;
+
+        }
+
+        console.log($scope.movimenti);
+      }).catch(function(error){
+        console.log(error);
+      });
+
+  }else{
+    $http.get(link1,{
+      params: {
+        id_utente:1,
+        id_tipo: $scope.id_tipo
+
+
+      }
+    }).then(function(response){
+      $scope.movimenti = response.data.movimenti;
+      var array= $scope.movimenti.filter(function(el){
+        var dat=new Date(el.data);
+        return dat.getUTCFullYear()==$scope.data.getUTCFullYear()&&
+        dat.getUTCMonth()==$scope.data.getUTCMonth()&&
+        dat.getUTCDate()==$scope.data.getUTCDate();
+      });
+      $scope.movimenti=array;
+      for (var i = 0; i < $scope.movimenti.length; i++) {
+        d[i]=$scope.movimenti[i].data.substring(0, 10);
+        o[i]=$scope.movimenti[i].data.substring(11, 16);
+        var parts = d[i].split("-"),
+        dateLong = new Date(+parts[0], parts[1]-1, +parts[2]);
+
+        var data=dateLong.toString().substring(0,15);
+        var ora=o[i];
+
+        $scope.movimenti[i].data=data+", "+ora;
+
+      }
+
+      console.log($scope.movimenti);
+    }).catch(function(error){
+      console.log(error);
+    });
+
   }
+  }
+
+
+
+  var ipObj1 = {
+     callback: function (val) {  //Mandatory
+       var da=new Date(val);
+       da.setMinutes(da.getMinutes() - da.getTimezoneOffset());
+       $scope.data= da;
+       console.log(da.getUTCDate());
+       console.log(da.getUTCMonth() + 1);
+     },
+     disabledDates: [],
+     dateFormat: 'dd MMMM yyyy',
+     //inputDate: new Date(),      //Optional
+     mondayFirst: false,          //Optional
+     closeOnSelect: false,       //Optional
+     templateType: 'popup'       //Optional
+   };
+
+   $scope.openDatePicker = function(){
+     ionicDatePicker.openDatePicker(ipObj1);
+   };
 
 
 
